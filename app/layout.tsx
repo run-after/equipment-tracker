ac2f4d1f18d55611198eff06cf5170219749a15d
Chase Stearn <53063763+run-after@users.noreply.github.com>
2023-10-23 15:33:54 -0400

    Add links to header in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,8 +14,33 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: ReactElement }) {
   return (
-    <html lang="en">
+    <html lang="en" className="bg-black text-white">
       <body className={inter.className}>
+        <header className="h-24 border-b border-white p-4 flex jusfify-between">
+          <a href="/" className="bg-red-500 w-12 h-12 mr-12 shrink-0"></a>
+          <ul className="flex text-lg w-full justify-end">
+            <li className="p-2">
+              <a href="/sites" className="hover:underline">
+                Sites
+              </a>
+            </li>
+            <li className="p-2">
+              <a href="/equipment" className="hover:underline">
+                Equipment
+              </a>
+            </li>
+            <li className="p-2">
+              <a href="/parts" className="hover:underline">
+                Parts
+              </a>
+            </li>
+            <li className="p-2">
+              <a href="/users" className="hover:underline">
+                Users
+              </a>
+            </li>
+          </ul>
+        </header>
         <FirebaseProvider>{children}</FirebaseProvider>
       </body>
     </html>

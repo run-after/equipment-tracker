b0a1bbbcad0ec1fc04469a506a8d1f56867dba5b
Chase Stearn <53063763+run-after@users.noreply.github.com>
2023-10-26 15:29:50 -0400

    Add boilerplate using modal

--- app/sites/page.tsx
+++ app/sites/page.tsx
@@ -1,3 +1,28 @@
+"use client";
+
+import { useState } from "react";
+
+import CreateSiteModal from "./CreateSiteModal";
+
 export default function Sites() {
-  return <main>Sites page</main>;
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <main className="flex flex-col gap-4 p-4">
+      <button
+        onClick={() => setShowModal(true)}
+        className="rounded p-4 bg-gray-600 text-gray-300 self-end"
+      >
+        Add new site
+      </button>
+      <div>
+        <ul>
+          <li>site 1</li>
+          <li>site 1</li>
+          <li>site 1</li>
+        </ul>
+      </div>
+      {showModal && <CreateSiteModal onClose={() => setShowModal(false)} />}
+    </main>
+  );
 }

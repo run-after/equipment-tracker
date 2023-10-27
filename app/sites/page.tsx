74438084b8461c2a035dc6d019f87017631a3033
Chase Stearn <53063763+run-after@users.noreply.github.com>
2023-10-27 15:47:09 -0400

    List sites from DB on load of /sites

--- app/sites/page.tsx
+++ app/sites/page.tsx
@@ -1,11 +1,55 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { collection, getDocs } from "firebase/firestore";
+
+import { useFirebase } from "@/app/contexts/firebase";
 
 import CreateSiteModal from "./CreateSiteModal";
 
+interface ISite {
+  GLC: string;
+  address1: string;
+  address2: string | null;
+  city: string;
+  state: string;
+  zip: string;
+  assignee: string;
+}
+
 export default function Sites() {
+  // Access context
+  const { db } = useFirebase();
+
+  // Local state
   const [showModal, setShowModal] = useState(false);
+  const [sites, setSites] = useState<ISite[]>([]);
+
+  const getSites = async () => {
+    try {
+      let arr: ISite[] = [];
+
+      // Get groups
+      const querySnapshot = await getDocs(collection(db, "sites"));
+
+      // Add to arr
+      querySnapshot.forEach((doc) => {
+        const site = doc.data();
+        arr.push(site as ISite);
+      });
+
+      setSites(arr);
+
+      // End loading
+      //setLoading(false);
+    } catch (e) {
+      console.log("e", e);
+    }
+  };
+
+  useEffect(() => {
+    getSites();
+  }, []);
 
   return (
     <main className="flex flex-col gap-4 p-4">
@@ -16,10 +60,15 @@ export default function Sites() {
         Add new site
       </button>
       <div>
-        <ul>
-          <li>site 1</li>
-          <li>site 1</li>
-          <li>site 1</li>
+        <h1 className="text-xl font-bold pb-4">Sites</h1>
+        <ul className="flex flex-col gap-2">
+          {sites.map((site) => (
+            <li key={site.GLC} className="border rounded p-2">
+              <a href={`/sites/${site.GLC}`} className="hover:underline">
+                {site.GLC}
+              </a>
+            </li>
+          ))}
         </ul>
       </div>
       {showModal && <CreateSiteModal onClose={() => setShowModal(false)} />}
